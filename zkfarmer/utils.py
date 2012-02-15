101771a74ee12bc1617e8c36cf9eb5b567846a79
Olivier Poitrey <rs@dailymotion.com>
2012-02-15 12:34:30 +0100

    Add missing file

---
+++ zkfarmer/utils.py
@@ -0,0 +1,40 @@
+import json
+import operator
+import logging
+
+
+def serialize(data):
+    try:
+        if type(data) != dict:
+            raise TypeError('Must be a dict')
+        return json.dumps(data)
+    except:
+        logging.warn('Cannot serialize: %s' % data)
+        return '{}'
+
+
+def unserialize(serialized):
+    try:
+        data = json.loads(serialized)
+        if type(data) != dict:
+            raise TypeError('Not a dict')
+        return data
+    except:
+        logging.warn('Cannot unserialize: %s' % serialized)
+        return {}
+
+
+def dict_get_path(the_dict, path):
+    try:
+        return reduce(operator.getitem, [the_dict] + path.split('.'))
+    except:
+        return None
+
+
+def dict_set_path(the_dict, path, value):
+    current = the_dict
+    for component in path.split('.')[:-1]:
+        if component not in current or type(current[component]) != dict:
+            current[component] = {}
+        current = current[component]
+    current[path.split('.')[-1]] = value
